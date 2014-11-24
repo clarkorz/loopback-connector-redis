8750f2b5cbae65b5459b9812ab63793a9c6e50e8
Clark Wang <clark.wangs@gmail.com>
2014-11-24 09:25:10 +0800

    restore test
    Signed-off-by: Clark Wang <clark.wangs@gmail.com>

--- test/init.js
+++ test/init.js
@@ -3,7 +3,7 @@ module.exports = require('should');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
 global.getSchema = function() {
-    var db = new DataSource(require('../'), { host: 'clarkorz.local', port: 6379 });
+    var db = new DataSource(require('../'));
     // db.log = function (a) { console.log(a); };
     return db;
 };
