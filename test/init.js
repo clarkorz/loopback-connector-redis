5cf26206641626c6d8a56539d94017c291675046
Clark Wang <clark.wangs@gmail.com>
2014-11-24 10:25:45 +0800

    allow init different type of redis connector
    Signed-off-by: Clark Wang <clark.wangs@gmail.com>

--- test/init.js
+++ test/init.js
@@ -2,8 +2,29 @@ module.exports = require('should');
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
-global.getSchema = function() {
-    var db = new DataSource(require('../'));
+var localSettings = tryRequire('./settings.local.json');
+
+global.getSchema = function(settings) {
+    var db = new DataSource(require('../'), defaults(settings, localSettings));
     // db.log = function (a) { console.log(a); };
     return db;
 };
+
+
+function tryRequire(path) {
+  try {
+    return require(path);
+  } catch (e) {
+    return {};
+  }
+}
+
+function defaults(dest, src) {
+  Object.keys(src).forEach(function (key) {
+    if (src.hasOwnProperty(key) && !dest.hasOwnProperty(key)) {
+      dest[key] = src[key];
+    }
+  });
+
+  return dest;
+}
