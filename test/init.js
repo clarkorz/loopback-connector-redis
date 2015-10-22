7858021a1dc8f7fc35ab14b7879f0866a4bc1520
Clark Wang <clark.wangs@gmail.com>
2015-10-22 10:46:53 +0800

    allow test config

--- test/init.js
+++ test/init.js
@@ -1,9 +1,21 @@
 module.exports = require('should');
 
+var _ = require('lodash');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
-global.getSchema = function() {
-    var db = new DataSource(require('../'), {});
+var localSettings = tryRequire('./settings.local.json');
+
+global.getSchema = function(settings) {
+    var db = new DataSource(require('../'), _.defaults({}, settings, localSettings));
     // db.log = function (a) { console.log(a); };
     return db;
 };
+
+
+function tryRequire(path) {
+  try {
+    return require(path);
+  } catch (e) {
+    return {};
+  }
+}
